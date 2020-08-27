d19da6e2f9910d09e57d38367cc91672b176186e
Breno Alves <brenoalv3ss@gmail.com>
2020-08-27 18:29:59 -0300

    Resoluçao do segundo  quesito bloco 4 aula 1 JS

---
+++ exercises/4_1/pratica_2.js
@@ -0,0 +1,8 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 8;
+let b = 3;
+
+numero = (a > b);
+
+console.log (numero);
