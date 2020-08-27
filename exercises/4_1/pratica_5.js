2a4786c751b7881cc3c92b3a272a997c7fe90fd2
Breno Alves <brenoalv3ss@gmail.com>
2020-08-27 20:30:27 -0300

    Resoluçao do quesito 5

---
+++ exercises/4_1/pratica_5.js
@@ -0,0 +1,10 @@
+// Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.
+
+let face1 = 100;
+let face2 = 50;
+let base = 30;
+
+somaTriangulo = (face1 + face2 + base);
+
+    console.log(somaTriangulo === 180);
+
