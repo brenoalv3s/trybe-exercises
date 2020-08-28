f05d8485bf411c0ae3756560450721f303bee618
Breno Alves <brenoalv3ss@gmail.com>
2020-08-28 20:07:17 -0300

    Algoritmo que decobre o menor valor condito em uma Array

---
+++ exercises/4_2/pratica_7.js
@@ -0,0 +1,13 @@
+// Utilizando o for, descubra qual o menor valor contido no array e imprima-o
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = Infinity;
+
+for (let index = 0; index < numbers.length; index++) {
+    
+    if (numbers [index] < menor ) {
+      menor = numbers[index]  
+    }   
+}
+
+console.log(menor)
