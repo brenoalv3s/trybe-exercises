4bdcc2fbfd4be64a183f52b458b71fb96f06448d
Breno Alves <brenoalv3ss@gmail.com>
2020-08-28 19:57:45 -0300

    Correçao dos codigos

--- exercises/4_2/pratica_3.js
+++ exercises/4_2/pratica_3.js
@@ -2,11 +2,22 @@
 
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 
 for (let index = 0; index < numbers.length; index++) {
     media += numbers[index] / numbers.length;
     
 }
-  console.log(media);
+  console.log(media);*/
+
+  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+let average = sum / numbers.length;
+
+console.log(average);
--- exercises/4_2/pratica_4.js
+++ exercises/4_2/pratica_4.js
@@ -1,7 +1,7 @@
 /* Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor 
 maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"; */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 
 for (let index = 0; index < numbers.length; index++) {
@@ -14,4 +14,19 @@ for (let index = 0; index < numbers.length; index++) {
       console.log(`Valor maior que 20`)
 } else {
     console.log(`Valor menor ou igual a 20`)
+}*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  result += numbers[i];
+}
+
+result = result / numbers.length;
+
+if (result > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
 }
