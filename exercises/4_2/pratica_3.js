042d28bea737145d695bc77e6dcb91c175ea41c9
Breno Alves <brenoalv3ss@gmail.com>
2020-08-28 19:32:47 -0300

    Algoritmo criado para descobrir o maior numero da array utilizando a funçao for e a condiçao if

--- exercises/4_2/pratica_3.js
+++ exercises/4_2/pratica_3.js
@@ -6,7 +6,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 
 for (let index = 0; index < numbers.length; index++) {
-    media += numbers[index] / 10;
+    media += numbers[index] / numbers.length;
     
 }
   console.log(media);
