2bef0d87a251c5c27f19ceec4b96d467578cf49e
Breno Alves <brenoalv3ss@gmail.com>
2020-08-31 18:52:21 -0300

    Soluçao primeiro quesito, criar um quadrado feito de asteriscos

--- exercises/4_3/pratica_1.js
+++ exercises/4_3/pratica_1.js
@@ -1,3 +1,8 @@
+/* Para o primeiro exercício de hoje, 
+faça um programa que, dado um valor n 
+qualquer, seja n > 1, imprima na tela 
+um quadrado feito de asteriscos de 
+lado de tamanho n.*/
 
 let n = 5;
 let lineIndex;
