47b565dcd9d434eb81f41355c23262dfadec2678
Breno Alves <brenoalv3ss@gmail.com>
2020-09-01 19:24:58 -0300

    simplificaçao do programa

--- exercises/4_4/pratica_fuction1.js
+++ exercises/4_4/pratica_fuction1.js
@@ -8,14 +8,17 @@ Crie uma função que receba uma string e retorne true se for um palíndromo, ou
     Retorno esperado: false
 */
 
-function verificaPalindrome(string) {
-    let reverse = string.split("").reverse().join("");
-    if (reverse === string) {
-      return true;
-    } else {
-      return false;
+function verificaPalindrome(palavra) {
+    let arrayLetras = palavra.split("");
+    let isPalindrome = true;
+    for (let index in arrayLetras) {
+      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+        isPalindrome = false;
+      }
     }
+    return isPalindrome;
   }
+
   
   console.log(verificaPalindrome('arara')); //true
   console.log(verificaPalindrome('desenvolvimento')); //false
