8248e7d3e9a09f63616b055208018b3226df4765
Breno Alves <brenoalv3ss@gmail.com>
2020-09-17 21:56:18 -0300

    Finalização do HTML e CSS

--- exercises/4_1/pratica_1.js
+++ exercises/4_1/pratica_1.js
@@ -1,14 +1,13 @@
 // Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para: adição, subtração, multiplicação divisão e módulo.
-
 let a = 10;
 let b = 5;
 
 // Soma
-
-soma = a+b;
-
-console.log(soma);
-
+function sum (a, b){
+return  a + b;
+}
+valorSoma = (5, 2)
+console.log(valorSoma)
 // Subtração
 
 sub = a-b;
