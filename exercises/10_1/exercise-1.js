8c53857f7ddd9daaa51c1a6ba29c8270b4cde9b5
Breno Alves <brenoalv3ss@gmail.com>
2020-10-29 19:31:45 -0300

    Exercise 2 and 3

--- exercises/10_1/exercise-1.js
+++ exercises/10_1/exercise-1.js
@@ -4,9 +4,7 @@
     Teste se o retorno de sum(0, 0) é 0
     Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
     Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")*/
-
-    const assert = require('assert');
-
+   
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('parameters must be numbers');
@@ -15,4 +13,4 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+module.exports = (sum);
