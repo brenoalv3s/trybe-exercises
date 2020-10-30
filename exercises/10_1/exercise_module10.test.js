5d8232e67c603b8d50041220be6238af9091c347
Breno Alves <brenoalv3ss@gmail.com>
2020-10-30 15:01:33 -0300

    exercise 5

--- exercises/10_1/exercise_module10.test.js
+++ exercises/10_1/exercise_module10.test.js
@@ -1,4 +1,4 @@
-const sum = require('./exercise-1.js', './exercise-2.js', './exercise-3.js');
+const sum = require('./exercise-1.js', './exercise-2.js', './exercise-3.js', './exercise-4.js', './exercise-5.js');
 
 describe('The sum (a, b) function returns the sum of parameter a and b', () => {
 
@@ -25,6 +25,9 @@ test('if the sum function throws an error when the parameters are 4 and "5" (str
 
 const myRemove = require('./exercise-2.js');
 const myRemoveWithoutCopy = require('./exercise-3.js');
+const myFizzBuzz = require('./exercise-4.js');
+const comparingObjects = require('./exercise-5.js');
+const objects = require('./exercise-5.js');
 
 describe('myRemove', () => {
   test('returns a new array without the item if the item belongs to it', () => {
@@ -65,4 +68,33 @@ describe('myRemoveWithoutCopy',() => {
 	it('Checks whether the function call returns the expected array', () => {
 		expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
 	});
+});
+
+describe('myFizzBuzz', () => {
+
+	test('if the number is divisible by 3 and 5', () => {
+		expect(myFizzBuzz(15)).toBe('fizzbuzz');
+ });
+
+ test('if the number is divisible by 3', () => {
+	expect(myFizzBuzz(9)).toBe('fizz');
+ });
+
+ test('if the number is divisible by 5', () => {
+	expect(myFizzBuzz(500)).toBe('buzz');
+ });
+
+ test('number that is not divisible by 3 or 5', () => {
+	 const num = 14;
+	expect(num % 3 === 0 && num % 5 === 0).toBeFalsy();
+ });
+
+ test('returns false if the argument is not a number', () => {
+	expect(myFizzBuzz(typeof num !== 'number')).toBeFalsy();
+ });
+});
+
+test("comparing objects", () => {
+  expect(comparingObjects.obj1 === comparingObjects.obj3).toBeTruthy();
+  expect(comparingObjects.obj1 !== comparingObjects.obj2).toBeFalsy();
 });
