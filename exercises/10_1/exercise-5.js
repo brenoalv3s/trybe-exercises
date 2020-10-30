ad460eae99e4c0c0db2011d672e4b8d29df9d714
Breno Alves <brenoalv3ss@gmail.com>
2020-10-30 15:02:35 -0300

    exercise 5

---
+++ exercises/10_1/exercise-5.js
@@ -0,0 +1,18 @@
+/* 5. Compare dois objetos (JSON) para verificar se são idênticos ou não */
+const comparingObjects = () => {
+const obj1 = {
+  city: "São Paulo",
+  state: "São Paulo"
+};
+
+const obj2 = {
+  city: "Campinas",
+  state: "São Paulo"
+};
+
+const obj3 = {
+  city: "São Paulo",
+  state: "São Paulo"
+};
+};
+module.exports = comparingObjects;
