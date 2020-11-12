ca5f416527f3122e4ff27c5895a84debaab863e1
Breno Alves <brenoalv3ss@gmail.com>
2020-11-12 18:53:09 -0300

    Exercício de fixação introdução React. Criação de componente, class, import, export

---
+++ fixation_exercise_React-11/fixation_11_1/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Breno Alves</h1>
+				<p>Estudante de Desenvolvimento Web - FullStack na Trybe, 27 anos, casado e CEO da Startup B&A</p>
+			</div>
+		);
+  }
+}
+
+export default Component;
